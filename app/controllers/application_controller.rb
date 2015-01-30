894e2d0ffb056baf754a51c958dbefbc3078f5e7
Tom Johnson <thomas.johnson@oregonstate.edu>
2015-01-30 09:20:36 -0800

    Update KriKri with generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Adds a few additional behaviors into the application controller 
-   include Blacklight::Controller
-  # Please be sure to impelement current_user and user_session. Blacklight depends on 
-  # these methods in order to perform user specific actions. 
+  # Adds a few additional behaviors into the application controller required
+  # by Blacklight. Note that Blacklight requires two specific methods:
+  # current_user and user_session to perform user-specific actions.
+  include Blacklight::Controller
 
-  layout 'blacklight'
+  # Use Krikri's layout by default
+  layout 'krikri/application'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
