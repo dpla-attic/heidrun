61bd30e1b05575aa1cb6101040e574e711d00159
Mark Breedlove <mb@dp.la>
2016-02-29 16:07:49 -0500

    Parameterize log_level configuration setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = Settings.log_level.to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
