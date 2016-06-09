2dc19f379b79912f5f3f086ba7ce5283cc7cbd97
Scott Williams <scott@dp.la>
2016-06-09 10:05:17 -0400

    Add #request to intial stubout with support passing base URIs Add uri_item to opts hash in #initalize Flushed out #get_identifier and removed comments

--- app/harvesters/loc_harvester.rb
+++ app/harvesters/loc_harvester.rb
@@ -10,11 +10,16 @@ class LocHarvester < Krikri::Harvesters::ApiHarvester
   #
   # @param opts [Hash] options for the harvester
   # @see .expected_opts
+  #
+  # The options for this harvester should be an array of URIs
+  #
   def initialize(opts = {})
     # TODO add defualt URI
     opts[:uri] ||= ''
     # TODO add name (check)
     opts[:name] ||= 'loc'
+    # Item URI
+    opts[:item_uri] ||= 'https://www.loc.gov/item/'
     super
   end
 
@@ -38,6 +43,7 @@ class LocHarvester < Krikri::Harvesters::ApiHarvester
     # TODO
     # Should be a call to /item/
 
+    response = request(opts[:item_uri] + identifier.to_s + '?fo=json')
     # response = request(:params => { :q => "id:#{identifier.to_s}" })
     # build_record(get_docs(response).first)
   end
@@ -52,15 +58,14 @@ class LocHarvester < Krikri::Harvesters::ApiHarvester
   #
   # @return [String] the provider's identifier for the document
   def get_identifier(doc)
-    # For /item/<id> requested
-    # [item][library_of_congress_control_number]
+    # doc is the response to a request for /item/<id>
+    doc[item][library_of_congress_control_number]
   end
 
   ##
   # @param response [Hash] a response from the LoC API
   #
   # @return [Integer] value of the number of records in the response
-  #
     def get_count(response)
       response['pagination']['of']
     end
@@ -115,6 +120,12 @@ class LocHarvester < Krikri::Harvesters::ApiHarvester
                         get_content(doc),
                         content_type)
   end
-end
+
+
+  ##
+  # Override request because it needs to support being passed different base URIs
+  def request(uri, request_opts)
+    JSON.parse(RestClient.get(uri, request_opts))
+  end
 
 end
